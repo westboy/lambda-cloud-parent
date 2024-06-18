4ba2b144f2e79cb594e841919ac72829e9eeadb5
熊猫de <Songxinwei1>
2024-06-18 17:35:12 +0800

    fix(BaseDo)：删除单例

--- jingfang-cloud-commons/jingfang-cloud-commons-core/src/main/java/com/jingfang/autoconfig/JacksonModuleConfigurer.java
+++ jingfang-cloud-commons/jingfang-cloud-commons-core/src/main/java/com/jingfang/autoconfig/JacksonModuleConfigurer.java
@@ -16,29 +16,19 @@ import java.time.LocalDateTime;
 @Configuration(proxyBeanMethods = false)
 public class JacksonModuleConfigurer {
 
-    // 单例bean声明
-    private static JavaTimeModule javaTimeModuleInstance;
-    private static SimpleModule simpleModuleInstance;
-
     @Bean
     public JavaTimeModule javaTimeModule() {
-        if (javaTimeModuleInstance == null) {
-            JavaTimeModule javaTimeModule = new JavaTimeModule();
-            javaTimeModule.addSerializer(LocalDateTime.class, new CustomLocalDateTimeSerializer());
-            javaTimeModule.addDeserializer(LocalDateTime.class, new CustomLocalDateTimeDeserializer());
-            javaTimeModuleInstance = javaTimeModule;
-        }
-        return javaTimeModuleInstance;
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new CustomLocalDateTimeSerializer());
+        javaTimeModule.addDeserializer(LocalDateTime.class, new CustomLocalDateTimeDeserializer());
+        return javaTimeModule;
     }
 
     @Bean
     public SimpleModule simpleModule() {
-        if (simpleModuleInstance == null) {
-            SimpleModule simpleModule = new SimpleModule();
-            simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
-            simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-            simpleModuleInstance = simpleModule;
-        }
-        return simpleModuleInstance;
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        return simpleModule;
     }
 }
