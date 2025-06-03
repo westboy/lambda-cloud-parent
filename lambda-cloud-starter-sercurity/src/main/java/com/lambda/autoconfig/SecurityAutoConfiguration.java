82991a49a3d4b8d11a538f8092325b926504aa06
jin <jin@westboy.net>
2025-06-03 21:05:57 +0800

    fix

--- lambda-cloud-starter-sercurity/src/main/java/com/lambda/autoconfig/SecurityAutoConfiguration.java
+++ lambda-cloud-starter-sercurity/src/main/java/com/lambda/autoconfig/SecurityAutoConfiguration.java
@@ -100,12 +100,6 @@ public class SecurityAutoConfiguration {
     public static class SaTokenConfiguration {
 
         private SecurityProperties securityProperties;
-        private SecureExtendInterceptor secureExtendInterceptor;
-
-        @Autowired(required = false)
-        public void setSaTokenCustomHandler(SecureExtendInterceptor secureExtendInterceptor) {
-            this.secureExtendInterceptor = secureExtendInterceptor;
-        }
 
         @Autowired
         public void setSecurityProperties(SecurityProperties securityProperties) {
@@ -120,18 +114,23 @@ public class SecurityAutoConfiguration {
         }
 
         @Bean
-        public WebMvcConfigurer saTokenWebMvcConfigurer() {
-            var secureInterceptor = new SecureInterceptor(secureExtendInterceptor);
-            var enableMethodAnnotation = securityProperties.getSaToken().getEnableMethodAnnotation();
-            var allIgnoreList = securityProperties.getSaToken().getAllIgnoreList();
+        @ConditionalOnBean(SecureExtendInterceptor.class)
+        public SaInterceptor saInterceptor(SecureExtendInterceptor secureExtendInterceptor) {
+            return new SaInterceptor(new SecureInterceptor(secureExtendInterceptor))
+                    .isAnnotation(securityProperties.getSaToken().getEnableMethodAnnotation());
+        }
+
+        @Bean
+        @ConditionalOnBean(SaInterceptor.class)
+        public WebMvcConfigurer saTokenWebMvcConfigurer(SaInterceptor saInterceptor) {
             return new WebMvcConfigurer() {
                 @SuppressWarnings("all")
                 @Override
                 public void addInterceptors(InterceptorRegistry interceptorRegistry) {
                     interceptorRegistry
-                            .addInterceptor(new SaInterceptor(secureInterceptor).isAnnotation(enableMethodAnnotation))
+                            .addInterceptor(saInterceptor)
                             .addPathPatterns("/**")
-                            .excludePathPatterns(allIgnoreList);
+                            .excludePathPatterns(securityProperties.getSaToken().getAllIgnoreList());
                 }
             };
         }
