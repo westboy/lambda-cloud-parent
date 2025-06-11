da60aa01a7eec218203978953b21cc4ef831b789
jiaoqy <1341185166@qq.com>
2025-06-11 09:43:20 +0800

    refactor(lambda-cloud-starter-sercurity): 重构短信认证过滤器逻辑
    - 优化了获取请求参数的逻辑，提高了代码的可读性和健壮性
    - 引入了新的方法来安全地获取请求参数，避免了潜在的空指针异常

--- lambda-cloud-starter-sercurity/src/main/java/com/lambda/security/web/sms/SmsAuthenticationProcessingFilter.java
+++ lambda-cloud-starter-sercurity/src/main/java/com/lambda/security/web/sms/SmsAuthenticationProcessingFilter.java
@@ -11,7 +11,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
+import java.util.function.Function;
+
 import org.apache.commons.collections4.MapUtils;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
@@ -42,22 +45,32 @@ public class SmsAuthenticationProcessingFilter extends AbstractAuthenticationPro
         if (!RequestMethod.POST.name().equals(request.getMethod())) {
             throw new AuthenticationException("Authentication method not supported: " + request.getMethod());
         }
-
-        Map<String, Object> smsLogin = getUserLoginForRequestBody(request);
-        if (MapUtils.isEmpty(smsLogin)) {
-            throw new AuthenticationException("Request body is empty");
-        }
-        String mobile = (String) smsLogin.getOrDefault(this.mobileParameter, "");
+        String mobile = safeObtain(this::obtainMobile, request);
+        String loginType = safeObtain(this::obtainLoginType, request);
+        String device = safeObtain(this::obtainDevice, request);
 
         if (StrUtil.isBlank(mobile)) {
-            throw new AuthenticationException("Mobile is empty");
+            Map<String, Object> smsLogin = getUserLoginForRequestBody(request);
+            if (MapUtils.isEmpty(smsLogin)) {
+                throw new AuthenticationException("Request body is empty");
+            }
+            mobile = (String) smsLogin.getOrDefault(this.mobileParameter, "");
+
+            if (StrUtil.isBlank(mobile)) {
+                throw new AuthenticationException("Mobile is empty");
+            }
+            request.setAttribute(mobileParameter, mobile);
+
+            if (StrUtil.isBlank(loginType)) {
+                loginType = (String) smsLogin.getOrDefault(this.loginTypeParameter, LoginType.ADMIN.getCode());
+                request.setAttribute(loginTypeParameter, loginType);
+            }
+
+            if (StrUtil.isBlank(device)) {
+                device = (String) smsLogin.getOrDefault(this.deviceParameter, "default");
+                request.setAttribute(deviceParameter, device);
+            }
         }
-
-        String loginType = (String) smsLogin.getOrDefault(this.loginTypeParameter, LoginType.ADMIN.getCode());
-        request.setAttribute(loginTypeParameter, loginType);
-
-        String device = (String) smsLogin.getOrDefault(this.deviceParameter, "default");
-        request.setAttribute(deviceParameter, device);
 
         LoginUser loginUser = userDetailService.loginByMobile(mobile, loginType);
 
@@ -67,4 +80,24 @@ public class SmsAuthenticationProcessingFilter extends AbstractAuthenticationPro
 
         return loginUser;
     }
+
+    private String obtainDevice(HttpServletRequest request) {
+        return request.getParameter(this.deviceParameter);
+    }
+
+    @Nullable
+    protected String obtainLoginType(HttpServletRequest request) {
+        return request.getParameter(this.loginTypeParameter);
+    }
+
+    @Nullable
+    protected String obtainMobile(HttpServletRequest request) {
+        return request.getParameter(this.mobileParameter);
+    }
+
+    private String safeObtain(Function<HttpServletRequest, String> extractor, HttpServletRequest request) {
+        String value = extractor.apply(request);
+        return value != null ? value : "";
+    }
+
 }
