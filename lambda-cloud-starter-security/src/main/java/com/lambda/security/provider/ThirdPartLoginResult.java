dac9a8f4a6eb8d5271e97c2c7c272139b0e42de9
jin <jin@westboy.net>
2025-07-12 12:40:27 +0800

    refactor(lambda-cloud-starter-security): 重构 ThirdPartLoginResult 类- 移除了 @AllArgsConstructor 和 @NoArgsConstructor 注解 - 将 body 字段类型从 Object 改为 String - 添加自定义构造函数，将 body 序列化为 JSON 字符串 - 优化 getBody 方法，直接使用 body 字符串进行反序列化

--- lambda-cloud-starter-security/src/main/java/com/lambda/security/provider/ThirdPartLoginResult.java
+++ lambda-cloud-starter-security/src/main/java/com/lambda/security/provider/ThirdPartLoginResult.java
@@ -1,27 +1,28 @@
 package com.lambda.security.provider;
 
 import com.lambda.cloud.core.utils.ClassTypeUtils;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import static com.lambda.cloud.core.Constants.GSON;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ThirdPartLoginResult {
     private String thirdType;
-    private Object body;
+    private String body;
+
+    public ThirdPartLoginResult(String thirdType, Object body) {
+        this.thirdType = thirdType;
+        this.body = GSON.toJson(body);
+    }
 
     public <T> T getBody(Class<T> clazz) {
         if (String.class.equals(clazz)) {
             return clazz.cast(body);
         }
         if (ClassTypeUtils.isPrimitiveOrWrapper(clazz)) {
-            Object result = ClassTypeUtils.convertPrimitiveOrWrapper(clazz, body.toString());
+            Object result = ClassTypeUtils.convertPrimitiveOrWrapper(clazz, body);
             return clazz.cast(result);
         }
-        return GSON.fromJson(body.toString(), clazz);
+        return GSON.fromJson(body, clazz);
     }
 }
