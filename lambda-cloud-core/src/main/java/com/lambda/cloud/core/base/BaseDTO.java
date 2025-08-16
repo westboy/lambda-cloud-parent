bc3f5b406bd619969ddda513ce06214527dc3bae
jin <jin@westboy.net>
2025-08-16 17:38:12 +0800

    feat(core): 在 BaseDTO 中添加 mapToString 方法
    - 在 BaseDTO 类中添加了 mapToString 方法，用于将 Map 对象转换为 JSON 字符串
    - 使用 Gson工具库进行序列化
    - 通过静态常量 GSON来复用 Gson 实例

--- lambda-cloud-core/src/main/java/com/lambda/cloud/core/base/BaseDTO.java
+++ lambda-cloud-core/src/main/java/com/lambda/cloud/core/base/BaseDTO.java
@@ -3,7 +3,12 @@ package com.lambda.cloud.core.base;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.gson.Gson;
 import com.lambda.cloud.core.convert.BaseConverter;
+import java.util.Map;
+import org.mapstruct.Named;
+
+import static com.lambda.cloud.core.Constants.GSON;
 
 /**
  * DTO 基类
@@ -24,5 +29,11 @@ public abstract class BaseDTO<D, E> {
     @JsonIgnore
     public LambdaQueryWrapper<E> getLambdaQueryWrapper() {
         return Wrappers.lambdaQuery();
+    }
+
+    @Named("mapToString")
+    protected String mapToString(Map<String, Object> map) {
+        if (map == null) return null;
+        return GSON.toJson(map);
     }
 }
