f6a0c14cf05b5c0732d5107a8cecb793bfeb1701
westboy <jin@westboy.net>
2025-10-07 19:51:18 +0800

    fix

---
+++ lambda-cloud-core/src/main/java/com/lambda/cloud/core/utils/ConvertUtils.java
@@ -0,0 +1,20 @@
+package com.lambda.cloud.core.utils;
+
+import com.lambda.cloud.core.convert.BaseConverter;
+import com.lambda.cloud.core.convert.ConverterResolver;
+
+
+/**
+ * ConvertUtils
+ *
+ * @author Jin
+ */
+public class ConvertUtils {
+
+
+    public static <T, S> T convert(S source) {
+        BaseConverter<S, T> converter = ConverterResolver.getConverter(source.getClass());
+        return converter.convertTo(source);
+    }
+
+}
