0d21f4f9b652106a6016649255f6f84f7f3d3713
westboy <jin@westboy.net>
2025-10-18 23:51:45 +0800

    fix: 修正TransactionRecordProtocolTest中日志记录的字段错误

--- lambda-cloud-starter-netty/src/test/java/com/lambda/cloud/netty/protocol/message/TransactionRecordProtocolTest.java
+++ lambda-cloud-starter-netty/src/test/java/com/lambda/cloud/netty/protocol/message/TransactionRecordProtocolTest.java
@@ -126,7 +126,7 @@ public class TransactionRecordProtocolTest {
         log.info("消费金额: {}", record.getInnerRecord().getTotalAmount());
         log.info("VIN: {}", record.getInnerRecord().getVIN());
         log.info("交易标识: {}", record.getInnerRecord().getTag());
-        log.info("交易日期、时间: {}", record.getInnerRecord().getTotalAmount());
+        log.info("交易日期、时间: {}", record.getInnerRecord().getTIME());
         log.info("停止原因: {}", record.getInnerRecord().getDesc());
         log.info("物理卡号: {}", record.getInnerRecord().getCard());
 
