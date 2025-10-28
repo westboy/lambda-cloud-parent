6466e163240db518ed71b7288ce3f77fd7c648ec
zx <jpjoo@vip.qq.com>
2025-10-28 17:35:08 +0800

    update: 修改 YkcV20BaseMessage 协议版本和字段调整

--- lambda-cloud-starter-ykc/src/main/java/com/lambda/cloud/ykc/message/v20/YkcV20BaseMessage.java
+++ lambda-cloud-starter-ykc/src/main/java/com/lambda/cloud/ykc/message/v20/YkcV20BaseMessage.java
@@ -3,18 +3,20 @@ package com.lambda.cloud.ykc.message.v20;
 import com.lambda.cloud.netty.protocol.annotation.ProtocolDataType;
 import com.lambda.cloud.netty.protocol.annotation.ProtocolField;
 import com.lambda.cloud.netty.protocol.annotation.ProtocolFrame;
+import com.lambda.cloud.ykc.message.ProtocolMessage;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
-@ProtocolFrame(frameType = "base", name = "云快充基础协议2.0", isPayload = true, description = "云快充2.0协议基础协议字段")
-public abstract class YkcV20BaseMessage<T> {
-
+@ProtocolFrame(frameType = "base", name = "云快充1.7协议", isPayload = true, description = "云快充1.7协议基础字段")
+public abstract class YkcV20BaseMessage<T> implements ProtocolMessage {
     @ProtocolField(order = 0, length = 1, dataType = ProtocolDataType.HEX, description = "起始符")
-    private String startFlag;
+    private String startFlag = "68";
 
     @ProtocolField(
             order = 1,
@@ -31,26 +33,35 @@ public abstract class YkcV20BaseMessage<T> {
             dataType = ProtocolDataType.HEX,
             computed = true,
             littleEndian = true,
-            description = "数据长度")
+            description = "序列号域")
     private Integer serialNumber;
 
     @ProtocolField(
             order = 3,
+            length = 7,
+            dataType = ProtocolDataType.CP56TIME2A,
+            computed = true,
+            littleEndian = true,
+            description = "发送时间")
+    private LocalDateTime postTime;
+
+    @ProtocolField(
+            order = 4,
             length = 1,
             dataType = ProtocolDataType.HEX,
             computed = true,
             littleEndian = true,
-            description = "数据长度")
-    private String encryption;
+            description = "加密标志")
+    private String encryptFlag;
 
-    @ProtocolField(order = 4, length = 1, dataType = ProtocolDataType.HEX, computed = true, description = "帧类型")
+    @ProtocolField(order = 5, length = 1, dataType = ProtocolDataType.HEX, computed = true, description = "帧类型")
     private String frameType;
 
-    @ProtocolField(order = 5, composite = true, computed = true, encryptedField = true, description = "内容")
-    private Object detail;
+    @ProtocolField(order = 6, composite = true, computed = true, description = "内容")
+    private T detail;
 
     @ProtocolField(
-            order = 6,
+            order = 7,
             length = 2,
             dataType = ProtocolDataType.HEX,
             littleEndian = true,
