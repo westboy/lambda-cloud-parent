d3f70c8b9f6d624a45c636e37f81f4784b6c4dfc
jin <jin@westboy.net>
2026-01-28 15:55:08 +0800

    feat(lambda-cloud-core): Add smart conversion method for lists and single objects

--- lambda-cloud-core/src/main/java/com/lambda/cloud/core/utils/ConvertUtils.java
+++ lambda-cloud-core/src/main/java/com/lambda/cloud/core/utils/ConvertUtils.java
@@ -2,6 +2,7 @@ package com.lambda.cloud.core.utils;
 
 import com.lambda.cloud.core.convert.BaseConverter;
 import com.lambda.cloud.core.convert.ConverterResolver;
+import java.util.List;
 
 /**
  * ConvertUtils
@@ -13,5 +14,22 @@ public class ConvertUtils {
     public static <T, S> T convert(S source) {
         BaseConverter<S, T> converter = ConverterResolver.getConverter(source.getClass());
         return converter.convertTo(source);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T, S> Object convertSmart(S source) {
+        if (source == null) {
+            return null;
+        }
+        if (source instanceof List<?> list) {
+            if (list.isEmpty()) {
+                return List.of();
+            }
+            BaseConverter<S, T> converter =
+                    ConverterResolver.getConverter(list.getFirst().getClass());
+
+            return converter.convertToList((List<S>) list);
+        }
+        return convert(source);
     }
 }
