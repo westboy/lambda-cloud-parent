7d25c8a38d13e94b113f6d9dda22906c7d8feaa0
jin <jin@westboy.net>
2026-03-11 22:09:21 +0800

    refactor: Reorder and update imports in AbstractEnvironmentPostProcessor

--- lambda-cloud-starter-web/src/main/java/com/lambda/cloud/web/AbstractEnvironmentPostProcessor.java
+++ lambda-cloud-starter-web/src/main/java/com/lambda/cloud/web/AbstractEnvironmentPostProcessor.java
@@ -1,10 +1,10 @@
 package com.lambda.cloud.web;
 
 import org.jspecify.annotations.NonNull;
+import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.boot.logging.DeferredLog;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -24,7 +24,8 @@ public abstract class AbstractEnvironmentPostProcessor
      * @param application
      */
     @Override
-    public void postProcessEnvironment(@org.jspecify.annotations.NonNull ConfigurableEnvironment environment, SpringApplication application) {
+    public void postProcessEnvironment(
+            @org.jspecify.annotations.NonNull ConfigurableEnvironment environment, SpringApplication application) {
         if (WebApplicationType.SERVLET.equals(application.getWebApplicationType())) {
             process(environment, application);
         }
